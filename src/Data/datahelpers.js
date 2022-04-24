92472dc0e3f3887b3a923538295eccc7f8bf2ed3
ufocharles <ufocharles@gmail.com>
2022-04-24 18:29:33 -0500

    clean up code
    clean up code

--- src/Data/datahelpers.js
+++ src/Data/datahelpers.js
@@ -2,27 +2,6 @@ import axios from 'axios'
 
 const baseURL = 'https://w0ayb2ph1k.execute-api.us-west-2.amazonaws.com/production?moves='
 
-// export async function getComputerPlay(moveArray) {
-//     let newData
-//     // axios.get(`${baseURL}[${moveArray.toString()}]`)
-//     //     .then(res => {
-//     //         console.log(res)
-//     //         newData.Data = res.data
-//     //         newData.Status = res.status
-//     //     }).catch(err => {
-//     //         newData.Data = err.response.data
-//     //         newData.Status = err.response.status
-//     //     })
-//     try {
-//         newData = axios.get(`${baseURL}[${moveArray.toString()}]`)
-//     } catch (err) {
-//         console.log(err.response)
-//     }
-//     console.log(newData)
-
-//     return newData
-// }
-
 export const getComputerPlay = (arrayMove) => {
     return new Promise((resolve, reject) => {
         try {
